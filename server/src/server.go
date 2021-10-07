b03c66c05e9f34723c3bc322df3d9e69f697c58a
Brad Davies <brad.davies@withwise.com>
2021-10-07 16:00:23 +0100

    Fix server

--- server/src/server.go
+++ server/src/server.go
@@ -9,10 +9,10 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
-			return
+		return
 	}
 
-	if r.Method != "POST" {
+	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusMethodNotAllowed)
 		return
 	}
